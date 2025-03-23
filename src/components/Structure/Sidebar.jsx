7754090c41c9e122e2cfb5fa8384e94da4d9771a
shramana263 <shramana652@gmail.com>
2025-03-23 21:59:43 +0530

    Enhance Sidebar component with dark mode support and improved accessibility

--- src/components/Structure/Sidebar.jsx
+++ src/components/Structure/Sidebar.jsx
@@ -15,26 +15,25 @@ import { useMobileContext } from "../../contexts/MobileContext";
 import { FaQuestionCircle } from "react-icons/fa";
 
 const Sidebar = ({ isOpen, setOpen }) => {
-
     const { setSidebarOpen } = usePanelContext()
     const { token } = useStateContext()
     const { isMobile } = useMobileContext()
 
     return (
         <>
-            <aside className="fixed top-0 left-0 z-40 w-64 h-screen animate-slideRight " >
-                <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 ">
+            <aside className="fixed top-0 left-0 z-40 w-64 h-screen animate-slideRight dark:bg-neutral-800" >
+                <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-neutral-800">
                     <ul className="space-y-2 font-medium">
                         <div className="p-3 w-full flex justify-end" onClick={() => { setOpen(false); setSidebarOpen(false) }}>
-                            <ImCross />
+                            <ImCross className="dark:text-white" />
                         </div>
                         {
                             !token &&
                             <li>
-                                <Link to='/landing' className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100 "
+                                <Link to='/landing' className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 dark:text-white dark:hover:bg-neutral-700"
                                     onClick={() => { if (isMobile) { setOpen(false) } }}
                                 >
-                                    <div><MdSpaceDashboard size={20} /></div>
+                                    <div className="dark:text-white"><MdSpaceDashboard size={20} /></div>
                                     <span className="ms-3">Home</span>
                                 </Link>
                             </li>
@@ -43,40 +42,39 @@ const Sidebar = ({ isOpen, setOpen }) => {
                             token &&
                             <>
                                 <li>
-                                    <Link to="profile" className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100 "
+                                    <Link to="profile" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 dark:text-white dark:hover:bg-neutral-700"
                                         onClick={() => { if (isMobile) { setOpen(false) } }}
                                     >
-                                        <div><FaUser size={20} /></div>
+                                        <div className="dark:text-white"><FaUser size={20} /></div>
                                         <span className="ms-3">Profile</span>
                                     </Link>
                                 </li>
                                 <li>
-                                    <Link to="/" className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100 "
+                                    <Link to="/" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 dark:text-white dark:hover:bg-neutral-700"
                                         onClick={() => { if (isMobile) { setOpen(false) } }}
                                     >
-                                        <div><MdSpaceDashboard size={20} /></div>
+                                        <div className="dark:text-white"><MdSpaceDashboard size={20} /></div>
                                         <span className="ms-3">Home</span>
                                     </Link>
                                 </li>
 
                                 <li>
-                                    <Link to="make-biodata" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+                                    <Link to="make-biodata" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 dark:text-white dark:hover:bg-neutral-700 group"
                                         onClick={() => { if (isMobile) { setOpen(false) } }}
                                     >
-                                        <div>
+                                        <div className="dark:text-white">
                                             <FaImagePortrait size={20} />
                                         </div>
                                         <span className="flex-1 ms-3 whitespace-nowrap">Biodata</span>
-                                        {/* <span class="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full ">3</span> */}
                                     </Link>
                                 </li>
                             </>
                         }
                         <li>
-                            <Link to="/review" className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100 "
+                            <Link to="/review" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 dark:text-white dark:hover:bg-neutral-700"
                                 onClick={() => { if (isMobile) { setOpen(false) } }}
                             >
-                                <div><MdRateReview size={20} /></div>
+                                <div className="dark:text-white"><MdRateReview size={20} /></div>
                                 <span className="ms-3">Reviews</span>
                             </Link>
                         </li>
@@ -84,75 +82,59 @@ const Sidebar = ({ isOpen, setOpen }) => {
                             !token &&
                             <>
                                 <li>
-                                    <Link to="signin" className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100 "
+                                    <Link to="signin" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 dark:text-white dark:hover:bg-neutral-700"
                                         onClick={() => { if (isMobile) { setOpen(false) } }}
                                     >
-                                        <div><BiLogInCircle size={20} /></div>
+                                        <div className="dark:text-white"><BiLogInCircle size={20} /></div>
                                         <span className="ms-3">Sign In</span>
                                     </Link>
                                 </li>
                                 <li>
-                                    <Link to="signup" className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100 "
+                                    <Link to="signup" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 dark:text-white dark:hover:bg-neutral-700"
                                         onClick={() => { if (isMobile) { setOpen(false) } }}
                                     >
-                                        <div><IoCreateOutline size={20} /></div>
+                                        <div className="dark:text-white"><IoCreateOutline size={20} /></div>
                                         <span className="ms-3">Sign Up</span>
                                     </Link>
                                 </li>
-
                             </>
-
                         }
 
                         <li>
-                            <Link to="/about-us" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+                            <Link to="/about-us" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 dark:text-white dark:hover:bg-neutral-700 group"
                                 onClick={() => { if (isMobile) { setOpen(false) } }}
                             >
-                                <div>
+                                <div className="dark:text-white">
                                     <IoMdInformationCircle size={20} />
                                 </div>
                                 <span className="flex-1 ms-3 whitespace-nowrap">About Us</span>
                             </Link>
                         </li>
                         <li>
-                            <Link to="/contact-us" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+                            <Link to="/contact-us" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 dark:text-white dark:hover:bg-neutral-700 group"
                                 onClick={() => { if (isMobile) { setOpen(false) } }}
                             >
-                                <div>
+                                <div className="dark:text-white">
                                     <RiContactsFill size={20} />
                                 </div>
                                 <span className="flex-1 ms-3 whitespace-nowrap">Contact Us</span>
                             </Link>
                         </li>
                         <li>
-                            <Link to="/frequently-asked-question" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+                            <Link to="/frequently-asked-question" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 dark:text-white dark:hover:bg-neutral-700 group"
                                 onClick={() => { if (isMobile) { setOpen(false) } }}
                             >
-                                <div>
-                                    <FaQuestionCircle  size={20} />
+                                <div className="dark:text-white">
+                                    <FaQuestionCircle size={20} />
                                 </div>
                                 <span className="flex-1 ms-3 whitespace-nowrap">FAQ</span>
                             </Link>
                         </li>
-                        {/* {
-                            token &&
-                            <>
-                                <li>
-                                    
-                                </li>
-                            </>
-                        } */}
-
-
                     </ul>
                 </div>
             </aside>
-
-
         </>
     );
 };
-
-
 
 export default Sidebar
